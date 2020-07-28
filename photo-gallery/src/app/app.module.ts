797e78271c2ba6da40e66e796424b6ef8c090d89
saianuragt <saianuragt@gmail.com>
2020-07-28 09:04:57 +0530

    FSD0620B105

---
+++ photo-gallery/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { GalleryComponent } from './gallery/gallery.component';
+import { TitleComponent } from './gallery/title/title.component';
+import { BodyComponent } from './gallery/body/body.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    GalleryComponent,
+    TitleComponent,
+    BodyComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
